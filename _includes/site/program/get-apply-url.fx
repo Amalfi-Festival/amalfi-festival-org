cece57d20b313670577170290b0c209ac4175d8e
legion80 <jonlee554@gmail.com>
2023-07-19 21:16:12 +0200

    local apply links scroll into view

--- _includes/site/program/get-apply-url.fx
+++ _includes/site/program/get-apply-url.fx
@@ -8,6 +8,8 @@ return: URL string
 {%- assign __return = site.apply | where: "slug", program.slug | first -%}
 {%- if __return -%}
     {%- assign __return = __return.url | relative_url -%}
+{%- elsif page.url == program.url -%}
+    {%- assign __return = "#how-to-apply" -%}
 {%- else -%}
     {%- assign __return = program.url | append: "#how-to-apply" | relative_url -%}
 {%- endif -%}
--- assets/site/site.js
+++ assets/site/site.js
@@ -115,4 +115,19 @@ window.addEventListener("DOMContentLoaded", function() {
   document.querySelectorAll('details').forEach((el) => {
     new Accordion(el);
   });
+
+
+  let anchorlinks = document.querySelectorAll('a[href^="#"]');
+  for (let item of anchorlinks) {
+    item.addEventListener('click', (e)=> {
+      let hashval = item.getAttribute('href')
+      let target = document.querySelector(hashval)
+      target.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+      })
+      history.pushState(null, null, hashval)
+      e.preventDefault()
+    })
+  }
 });
