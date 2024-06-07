5a65bca5b870d47f0adabca4e7d0632538c1cfbe
legion80 <jonlee554@gmail.com>
2024-06-07 11:25:19 -0700

    Support new PayPal form for balance only
    And add capability to have third-party JS includes in |custom-js|. Restore the old payment path for registrations.

--- assets/site/payment-new.js
+++ assets/site/payment-new.js
@@ -4,23 +4,19 @@ function payTypeChanged() {
         return;
 
     document.getElementById("formContainer").className = payType;
-    document.getElementById("formSelectionContainer").className = payType;
-
+    document.getElementById("formSelectionContainer").className = document.forms[0].payType.value;
     loadPayPalForm(payType);
 }
 
 function programChanged(e) {
     var slugOfSelectedProgram = document.querySelector('#programContainer input[name="os0"]:checked');
-    if (!slugOfSelectedProgram)
-        return;
     slugOfSelectedProgram = slugOfSelectedProgram.dataset.slug;
-    document.getElementById("formRegistration").className = `standard-block form ${slugOfSelectedProgram}`;
-    inputChanged(e);
+    document.getElementById("formRegistration").className = `standard-block ${slugOfSelectedProgram}`;
 }
 
 function loadPayPalForm(payType) {
     let paypalForm = document.querySelector(`#${payType} .paypalForm`);
-    if (paypalForm.hostedButtonFormLoaded)
+    if (!paypalForm || paypalForm.hostedButtonFormLoaded)
         return;
     let hostedButtonId = paypalForm.dataset.hostedButtonId;
     paypal.HostedButtons({
@@ -32,40 +28,11 @@ function loadPayPalForm(payType) {
 function inputChanged(e) {
     let input = e.target;
     let parentNode = input.closest(".form");
-
-    let validity = e.target.validity;
-    if (validity.valueMissing) {
-        input.setCustomValidity("Please fill out this field");
-        input.reportValidity();
-        parentNode.querySelector(".paypalForm").classList.add("disabled");
+    if (!parentNode)
         return;
-    } else if (validity.patternMismatch) {
-        input.setCustomValidity("Please enter a valid balance amount in USD");
-        input.reportValidity();
-        parentNode.querySelector(".paypalForm").classList.add("disabled");
-        return;
-    }
-
-    input.setCustomValidity("");
 
     let name = parentNode.querySelector("#memo");
-    let amalfiInput = parentNode.querySelector(".amalfi-input.name");
-    name.value = amalfiInput.value;
-
     let amount = parentNode.querySelector("#amount");
-    amalfiInput = parentNode.querySelector(".amalfi-input.cost");
-    if (amalfiInput)
-        amount.value = +amalfiInput.value;
-    else {
-        let slugOfSelectedProgram = document.querySelector('#programContainer input[name="os0"]:checked');
-        if (slugOfSelectedProgram) {
-            slugOfSelectedProgram = slugOfSelectedProgram.dataset.slug;
-            amalfiInput = document.getElementById(`invoice-${slugOfSelectedProgram}`);
-            amount.value = +amalfiInput.dataset.total;
-        } else {
-            amount.value = 0;
-        }
-    }
 
     if (name.value && amount.value > 0)
         parentNode.querySelector(".paypalForm").classList.remove("disabled");
@@ -76,9 +43,6 @@ function inputChanged(e) {
 window.addEventListener("DOMContentLoaded", function() {
     document.getElementById("payTypeContainer").addEventListener("click", payTypeChanged);
     document.getElementById("programContainer").addEventListener("click", programChanged);
-
-    let inputs = document.querySelectorAll(".amalfi-input");
-    inputs.forEach(input => input.addEventListener("input", inputChanged));
 
     var search = new URLSearchParams(location.search);
     if (search.get("registration")) {
