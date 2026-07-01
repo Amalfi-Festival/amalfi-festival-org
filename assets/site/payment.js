873f48a4ac7818d88a8c78ec486f739ddfb722bb
legion80 <jonlee554@gmail.com>
2026-07-01 12:44:31 -0700

    Refactor PayPal payment form
    PayPal's button support has been updated enough now that we can have inline forms for both registration and balance. This sort of ruins however where the listing of the invoice is. Right now the invoice is before the drop-down selection of the program. It doesn't quite make sense, and for now we will remove it, but this represents the latest in that development.
    
    Some things to note are that this needs assumed support loading based on query string. I think payment of balance and registration is possible, but not selection of the program since that is part of the hosted button on PayPal.

--- assets/site/payment.js
+++ assets/site/payment.js
@@ -3,53 +3,44 @@ function payTypeChanged() {
     if (!payType)
         return;
 
-    document.getElementById("formContainer").className = payType;
-    document.getElementById("formSelectionContainer").className = document.forms[0].payType.value;
+    document.body.classList.remove("formRegistration", "formBalance");
+    document.body.classList.add(payType);
     loadPayPalForm(payType);
 }
 
-function programChanged(e) {
-    var slugOfSelectedProgram = document.querySelector('#programContainer input[name="os0"]:checked');
-    slugOfSelectedProgram = slugOfSelectedProgram.dataset.slug;
-    document.getElementById("formRegistration").className = `standard-block ${slugOfSelectedProgram}`;
+function programButtonClicked(e) {
+    var slugOfSelectedProgram = e.target.dataset.slug;
+    document.getElementById("registrationFees").className = `standard-block ${slugOfSelectedProgram}`;
 }
 
 function loadPayPalForm(payType) {
-    let paypalForm = document.querySelector(`#${payType} .paypalForm`);
-    if (!paypalForm || paypalForm.hostedButtonFormLoaded)
-        return;
-    let hostedButtonId = paypalForm.dataset.hostedButtonId;
+    let paypalContainer = document.getElementById("paypalFormContainer");
+    paypalContainer.replaceChildren();
+
+    let formContainer = document.createElement("div");
+    const hostedButtonId = payType == "formRegistration" ? "5PEGZF49YR5G2" : "LCQMKDH2KKVC6";
+    const formId = `paypalForm-${hostedButtonId}`;
+    formContainer.setAttribute("id", formId);
+    paypalContainer.append(formContainer);
+
     paypal.HostedButtons({
-        hostedButtonId: hostedButtonId
-    }).render(`#paypal-container-${hostedButtonId}`);
-    paypalForm.hostedButtonFormLoaded = true;
-}
-
-function inputChanged(e) {
-    let input = e.target;
-    let parentNode = input.closest(".form");
-    if (!parentNode)
-        return;
-
-    let name = parentNode.querySelector("#memo");
-    let amount = parentNode.querySelector("#amount");
-
-    if (name.value && amount.value > 0)
-        parentNode.querySelector(".paypalForm").classList.remove("disabled");
-    else
-        parentNode.querySelector(".paypalForm").classList.add("disabled");
+        hostedButtonId: hostedButtonId,
+        onInit: (data, actions) => {
+            alert('hie');
+        }
+    }).render(`#${formId}`);
 }
 
 window.addEventListener("DOMContentLoaded", function() {
     document.getElementById("payTypeContainer").addEventListener("click", payTypeChanged);
-    document.getElementById("programContainer").addEventListener("click", programChanged);
+    document.getElementById("programContainer").addEventListener("click", programButtonClicked);
 
     var search = new URLSearchParams(location.search);
     if (search.get("registration")) {
         document.getElementById("payTypeRegistration").checked = true;
         document.getElementById(`program-${search.get("registration")}`).checked = true;
         payTypeChanged();
-        programChanged();
+        programButtonClicked();
     } else if (search.get("balance")) {
         document.getElementById("payTypeBalance").checked = true;
         payTypeChanged();
